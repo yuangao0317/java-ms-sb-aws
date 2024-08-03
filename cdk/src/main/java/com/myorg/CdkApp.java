6d56e45c6882cfb8e83b51b9b3ac8a2d31ab8f5f
YuanGao0317 <yuangao0317@gmail.com>
2024-08-02 23:25:56 -0400

    add product-service ecr repo to aws

--- cdk/src/main/java/com/myorg/CdkApp.java
+++ cdk/src/main/java/com/myorg/CdkApp.java
@@ -1,15 +1,32 @@
 package com.myorg;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CdkApp {
     public static void main(final String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
         App app = new App();
 
+        Environment environment = Environment.builder()
+                .account(dotenv.get("AWS_ACCOUNT"))
+                .build();
+
+        Map<String, String> infraTags = new HashMap<>();
+        infraTags.put("team", "java-ms-sb-aws");
+        infraTags.put("cost", "java-ms-sb-aws-ecr-infrastructure");
+
+        new ECRStack(app, "ECR-Stack", StackProps.builder()
+                .env(environment)
+                .tags(infraTags)
+                .build());
 
         app.synth();
     }
