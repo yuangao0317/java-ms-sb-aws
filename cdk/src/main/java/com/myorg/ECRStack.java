b22b9506c5f860d4d070415000e2333c1c2cf982
YuanGao0317 <yuangao0317@gmail.com>
2024-08-03 23:00:12 -0400

    add vpc stack

--- cdk/src/main/java/com/myorg/ECRStack.java
+++ cdk/src/main/java/com/myorg/ECRStack.java
@@ -1,5 +1,6 @@
 package com.myorg;
 
+import org.jetbrains.annotations.Nullable;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -11,10 +12,10 @@ import software.constructs.Construct;
 public class ECRStack extends Stack {
     private final Repository productServiceRepo;
 
-    public ECRStack(final Construct scope, final String id, final StackProps props) {
+    public ECRStack(@Nullable final Construct scope, @Nullable final String id, @Nullable final StackProps props) {
         super(scope, id, props);
 
-        this.productServiceRepo = new Repository(this, "ProductService", RepositoryProps.builder()
+        this.productServiceRepo = new Repository(this, "Product-Service", RepositoryProps.builder()
                 .repositoryName("product-service")
                 .removalPolicy(RemovalPolicy.DESTROY) // delete with ecr stack
                 .imageTagMutability(TagMutability.IMMUTABLE) // overwrite version on changes
---
+++ cdk/src/main/java/com/myorg/VPCStack.java
@@ -0,0 +1,27 @@
+package com.myorg;
+
+import org.jetbrains.annotations.Nullable;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcProps;
+import software.constructs.Construct;
+
+public class VPCStack extends Stack {
+    private final Vpc vpc;
+
+    public VPCStack(@Nullable final Construct scope, @Nullable final String id, @Nullable final StackProps props) {
+        super(scope, id, props);
+
+        this.vpc = new Vpc(this, "VPC-Stack", VpcProps.builder()
+                .vpcName("vpc-stack")
+                //.maxAzs(2)
+                //DO NOT DO THIS IN PRODUCTION!!!
+                //.natGateways(0)
+                .build());
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+}
