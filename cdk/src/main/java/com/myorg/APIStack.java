f4ef91cfb6da0ccd78d4ee800fa4120c5f19cb27
YuanGao0317 <yuangao0317@gmail.com>
2024-08-07 22:08:54 -0400

    add methods for /products

--- cdk/src/main/java/com/myorg/APIStack.java
+++ cdk/src/main/java/com/myorg/APIStack.java
@@ -7,6 +7,9 @@ import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
 import software.constructs.Construct;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class APIStack extends Stack {
     public APIStack(@Nullable final Construct scope,
                     @Nullable final String id,
@@ -39,7 +42,83 @@ public class APIStack extends Stack {
                         )
                         .build())
         );
+
+        // POST /products
+        productsResource.addMethod("POST", new Integration(
+                IntegrationProps.builder()
+                        .type(IntegrationType.HTTP_PROXY)
+                        .integrationHttpMethod("POST")
+                        .uri("http://" + apiStackDependency.nlb().getLoadBalancerDnsName() + ":8080/api/products")
+                        .options(
+                                IntegrationOptions.builder()
+                                        .vpcLink(apiStackDependency.vpcLink())
+                                        .connectionType(ConnectionType.VPC_LINK)
+                                        .build()
+                        )
+                        .build())
+        );
+
+        // GET /products/{id}
+        String productId = "{id}";
+
+        Map<String, String> productIdIntegrationParameters = new HashMap<>();
+        productIdIntegrationParameters.put("integration.request.path.id", "method.request.path.id");
+
+        Map<String, Boolean> productIdMethodParameters = new HashMap<>();
+        productIdMethodParameters.put("method.request.path.id", true);
+
+        Resource productsIdResource = productsResource.addResource(productId);
+
+        productsIdResource.addMethod("GET", new Integration(
+                IntegrationProps.builder()
+                        .type(IntegrationType.HTTP_PROXY)
+                        .uri("http://" + apiStackDependency.nlb().getLoadBalancerDnsName() + ":8080/api/products" + String.format("/%s", productId))
+                        .options(
+                                IntegrationOptions.builder()
+                                        .vpcLink(apiStackDependency.vpcLink())
+                                        .connectionType(ConnectionType.VPC_LINK)
+                                        .requestParameters(productIdIntegrationParameters)
+                                        .build())
+                        .build()),
+                MethodOptions.builder().requestParameters(productIdMethodParameters).build()
+        );
+
+
+        // PUT /products/{id}
+        productsIdResource.addMethod("PUT", new Integration(
+                        IntegrationProps.builder()
+                                .type(IntegrationType.HTTP_PROXY)
+                                .uri("http://" + apiStackDependency.nlb().getLoadBalancerDnsName() + ":8080/api/products" + String.format("/%s", productId))
+                                .options(
+                                        IntegrationOptions.builder()
+                                                .vpcLink(apiStackDependency.vpcLink())
+                                                .connectionType(ConnectionType.VPC_LINK)
+                                                .requestParameters(productIdIntegrationParameters)
+                                                .build())
+                                .build()),
+                MethodOptions.builder().requestParameters(productIdMethodParameters).build()
+        );
+
+        // DELETE /products/{id}
+        productsIdResource.addMethod("DELETE", new Integration(
+                        IntegrationProps.builder()
+                                .type(IntegrationType.HTTP_PROXY)
+                                .uri("http://" + apiStackDependency.nlb().getLoadBalancerDnsName() + ":8080/api/products" + String.format("/%s", productId))
+                                .options(
+                                        IntegrationOptions.builder()
+                                                .vpcLink(apiStackDependency.vpcLink())
+                                                .connectionType(ConnectionType.VPC_LINK)
+                                                .requestParameters(productIdIntegrationParameters)
+                                                .build())
+                                .build()),
+                MethodOptions.builder().requestParameters(productIdMethodParameters).build()
+        );
+
     }
+
+
+
+    private void
 }
 
 record APIStackDependency(
